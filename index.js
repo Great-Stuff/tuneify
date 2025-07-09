7e6ce6073fe50b5cedc02a6e7df359231f5bf20b
Great-Stuff <124748279+Great-Stuff@users.noreply.github.com>
2025-07-09 15:08:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const bottomSection = document.querySelector("#bottomSection")
+const bottomSectionWrapper = document.querySelector("#bottomSectionWrapper")
+const nav = document.querySelector("#nav")
+
+function setBottomSectionHeight(nav, wrapper){
+    var navBoundingClientTop = nav.getBoundingClientRect().top
+    var wrapperBoundingClientTop = wrapper.getBoundingClientRect().top
+    var setHeight = navBoundingClientTop - wrapperBoundingClientTop
+    wrapper.style["height"] = (setHeight-32) + "px"
+}
+
 const APIController = (function(){
     const clientID = ""
     const clientSecret = ""
 })
+
+document.addEventListener('reload', setBottomSectionHeight(nav, bottomSectionWrapper))
